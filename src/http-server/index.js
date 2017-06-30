a9ece1c3118e8504f3cc5bc731536f03e1514645
Jesse Tane <jesse.tane@gmail.com>
2017-06-30 13:59:22 -0400

    add some debugging

--- src/http-server/index.js
+++ src/http-server/index.js
@@ -6,10 +6,18 @@ var toKML = require('../to-kml')
 var server = new http.Server()
 
 server.on('request', (req, res) => {
+  // console.log(req.method + ' ' + req.url, req.headers)
+  var pathname = req.url.split('?')[0]
+  if (pathname !== '/') {
+    res.statusCode = 404
+    res.end('not found')
+    return
+  }
   fetchSpreadsheet(process.env.GOOGLE_SHEET, (err, objects) => {
     if (err) {
       res.statusCode = 500
-      return res.end(err.message)
+      res.end(err.message)
+      return
     }
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET')
@@ -18,5 +26,6 @@ server.on('request', (req, res) => {
 })
 
 server.listen(process.env.PORT || 8080, err => {
+  if (err) throw err
   console.log('server listening on ' + server.address().port)
 })
